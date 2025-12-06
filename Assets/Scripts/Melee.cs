568c2233668c887332aaf0e398bc96bd9057fcd2
Neal <nealprajapati22@gmail.com>
2025-12-06 12:49:48 -0500

    Fixed Crashing when attacking Projective, and Added Destroying Projectiles on Hit

--- Assets/Scripts/Melee.cs
+++ Assets/Scripts/Melee.cs
@@ -5,7 +5,9 @@ using System.Collections;
 public class Melee : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Enemy")) {
+        if (!other.TryGetComponent<Projectile>(out _)) {
+            Destroy(other);
+        } else if (other.CompareTag("Enemy") && !other.TryGetComponent<Projectile>(out _)) {
             other.GetComponent<Enemy>().TakeDamage((int)Player.Instance.baseMeleeDamage + (int)Player.Instance.damageMeleeModifier);
         }
     }
