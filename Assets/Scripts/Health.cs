463deaf6c0fdb852a886335ca209d769c8e844ab
Neal <nealprajapati22@gmail.com>
2025-12-07 13:44:27 -0500

    Clean Up Code and Fixed Black Hearts Spawning Over Red

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -43,6 +43,18 @@ public class Health : MonoBehaviour {
         health = Player.Instance.health;
         maxHealth = Player.Instance.maxHealth;
         for (int i = 1; i <= 10; i++) {
+            if ((!GameObject.Find("blackheart"+i)) && (maxHealth >= i)) {
+                GameObject heartObject = new GameObject("blackheart" + i);
+                heartObject.transform.SetParent(HUD.transform, false);
+                Image heartSprite = heartObject.AddComponent<Image>();
+                heartSprite.sprite = heart;
+                heartSprite.color = Color.black;
+                RectTransform rt = heartObject.GetComponent<RectTransform>();
+                rt.anchoredPosition = new Vector2(-412f  + i * 28, 214.6f);
+                rt.localScale = new Vector3(0.26f, 0.26f, 0.26f);
+            }
+        }
+        for (int i = 1; i <= 10; i++) {
             if ((!GameObject.Find("heart"+i)) && (health >= i)) {
                 GameObject heartObject = new GameObject("heart" + i);
                 heartObject.transform.SetParent(HUD.transform, false);
@@ -54,18 +66,6 @@ public class Health : MonoBehaviour {
             } else if ((GameObject.Find("heart"+i)) && (health < i)) {
                 GameObject destroyHeart = GameObject.Find("heart"+i);
                 GameObject.Destroy(destroyHeart);
-            }
-        }
-        for (int i = 1; i <= 10; i++) {
-            if ((!GameObject.Find("blackheart"+i)) && (maxHealth >= i)) {
-                GameObject heartObject = new GameObject("blackheart" + i);
-                heartObject.transform.SetParent(HUD.transform, false);
-                Image heartSprite = heartObject.AddComponent<Image>();
-                heartSprite.sprite = heart;
-                heartSprite.color = Color.black;
-                RectTransform rt = heartObject.GetComponent<RectTransform>();
-                rt.anchoredPosition = new Vector2(-412f  + i * 28, 214.6f);
-                rt.localScale = new Vector3(0.26f, 0.26f, 0.26f);
             }
         }
     }
--- Assets/Scripts/Melee.cs
+++ Assets/Scripts/Melee.cs
@@ -5,12 +5,9 @@ using System.Collections;
 public class Melee : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("I Am Here " + other.CompareTag("Enemy") + " " + other.TryGetComponent<Projectile>(out _));
         if (other.CompareTag("Enemy") && other.TryGetComponent<Projectile>(out _)) {
-            Debug.Log("Destroyed Projectile Hopefully");
             Destroy(other.gameObject);
         } else if (other.CompareTag("Enemy") && !other.TryGetComponent<Projectile>(out _)) {
-            Debug.Log("Hit Enemy");
             other.GetComponent<Enemy>().TakeDamage((int)Player.Instance.baseMeleeDamage + (int)Player.Instance.damageMeleeModifier);
         }
     }
